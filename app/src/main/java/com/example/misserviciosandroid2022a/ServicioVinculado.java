09f8492a8430aca59aca83262f7fd074cba1396a
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2022-01-24 12:00:50 -0600

    Se finalizo el servicio vinculado

--- app/src/main/java/com/example/misserviciosandroid2022a/ServicioVinculado.java
+++ app/src/main/java/com/example/misserviciosandroid2022a/ServicioVinculado.java
@@ -8,23 +8,42 @@ import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
-public class ServicioVinculado extends Service {
-    private final IBinder binder = new MyBinder();
+import java.util.Random;
 
+
+public class ServicioVinculado extends Service {
+    // Binder given to clients
+    private final IBinder binder = new MyBinder();
+    // Random number generator
+    private final Random mGenerator = new Random();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
     public class  MyBinder extends Binder{
-        ServicioVinculado getService(){
+        ServicioVinculado getService()
+        {
             return  ServicioVinculado.this;
         }
     }
-
     @Nullable
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
         return binder;
     }
 
-    public void metodoInterfaceCompoSer(){
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
 
+    /** method for clients
+     * Metodo interface para interactuar con el servicio
+     * */
+    public int getNumeroAleatorio() {
+        return mGenerator.nextInt(100);
     }
 
 }
